94bdfc594cf763f3edf562ac2a7f9d4358a94ec2
pao15-ui <martinsanchez2026262@gmail.com>
2026-05-21 14:45:00 -0500

    Actividad semana4

---
+++ semana4/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res) =>{
+ res.send('servidor  funcionando correctamente');
+});
+app.listen(3000,()=>{
+ console.log('servidor ejecutandose en puerto 3000');
+});
